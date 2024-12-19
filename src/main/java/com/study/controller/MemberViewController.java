09c5aa4cd138cbd8dac02e08c7812436720af0ba
hhongyeahh <hhongyeahh@gmail.com>
2024-12-20 00:36:47 +0900

    #27 - [Week10] 회원가입에 보안 추가하기 (2)
    - MemberViewController : 회원가입 폼 데이터 처리, 회원가입 API 정의, MemberCommandService 사용하여 회원가입 로직 수행, 예외처리 담당
    - MemberCommandServiceImpl : 회원가입 로직에서 비밀번호 암호화 저장 수행 위해 PasswordEncoder 사용
    - SecurityConfig : /member/signup 경로 허용하도록 설정 추가 -> 로그인 하지 않아도 새로 생성한 회원가입 API 호출 가능

--- src/main/java/com/study/controller/MemberViewController.java
+++ src/main/java/com/study/controller/MemberViewController.java
@@ -17,24 +17,24 @@ public class MemberViewController {
 
     private final MemberCommandService memberCommandService;
 
-//    @PostMapping("/members/signup")
-//    @Operation(summary = "타임리프와 스프링 시큐리티 사용한 회원가입 API")
-//    public String joinMember(@ModelAttribute("memberJoinDto")MemberRequestDTO.JoinDto request,
-//                             BindingResult bindingResult,
-//                             Model model){
-//        if(bindingResult.hasErrors()){
-//            //뷰에 데이터 바인딩 실패 시 -> signup 페이지 유지
-//            return "singup";
-//        }
-//        try{
-//            memberCommandService.joinMember(request);
-//            return "redirect:/login";
-//        }catch (Exception e){
-//            //회원가입 과정에서 에러 발생 -> 에러 메시지 + signup 페이지 유지
-//            model.addAttribute("error",e.getMessage());
-//            return "signup";
-//        }
-//    }
+    @PostMapping("/members/signup")
+    @Operation(summary = "타임리프와 스프링 시큐리티 사용한 회원가입 API")
+    public String joinMember(@ModelAttribute("memberJoinDto")MemberRequestDTO.JoinDto request,
+                             BindingResult bindingResult,
+                             Model model){
+        if(bindingResult.hasErrors()){
+            //뷰에 데이터 바인딩 실패 시 -> signup 페이지 유지
+            return "singup";
+        }
+        try{
+            memberCommandService.joinMember(request);
+            return "redirect:/login";
+        }catch (Exception e){
+            //회원가입 과정에서 에러 발생 -> 에러 메시지 + signup 페이지 유지
+            model.addAttribute("error",e.getMessage());
+            return "signup";
+        }
+    }
 
     @GetMapping("/login")
     public String loginPage(){
