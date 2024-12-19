bd66c941b743d5e3ac6e5392f9db5c2ccccfea94
hhongyeahh <hhongyeahh@gmail.com>
2024-12-19 23:58:52 +0900

    #27 - [Week10] SpringConfig 클래스 생성
    - 애플리케이션의 보안 정책을 정의하는 클래스
    - FilterChain 사용하여 들어오는 HTTP 요청을 필터링 -> 인증 인가 로직 처리

---
+++ src/main/java/com/study/config/security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.study.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity //Spring security 설정 활성화 -> customize한 보안 설정이 기본 설정 보다 우선 적용됨
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/","/home","/signup","/css/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home",true)
+                        .permitAll()
+                )
+                .logout((logout) ->logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                );
+        return http.build();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
